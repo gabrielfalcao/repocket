f382378c3a36908fb57576586de4cb0b3ecb6819
Gabriel Falcão <gabriel@nacaolivre.org>
2015-08-31 03:52:00 -0400

    Release 0.1.7

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ requirements = [
 
 setup(
     name='repocket',
-    version='0.1.6',
+    version='0.1.7',
     description='simple active record for redis',
     entry_points={
         'console_scripts': ['repocket = repocket.cli:main'],
